8b1008acb2de3f015cf723fa515e8e280fc6ed65
Weronika18991 <s18991@pjwstk.edu.pl>
2020-03-13 23:32:44 +0100

    p

---
+++ Cwiczenia2_apbd/Cwiczenia2_apbd/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cw2_apbd
+{
+   public class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
